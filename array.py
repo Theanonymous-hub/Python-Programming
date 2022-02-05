cdf62d8c4186749b56cedb1e57815c7367b1339c
Prasana.R <76907455+Theanonymous-hub@users.noreply.github.com>
2022-02-05 13:00:56 +0530

    Update array.py

--- array.py
+++ array.py
@@ -1,3 +1,8 @@
+#You are given with two arrays .Your task is to merge the array such that first array is assending order and second is descending order.
+#Input description
+#First line contains two integer'n' and'm'.'n' denotes length of array 1 and 'm' of array 2.
+#Next line contains 'm' space seperated numbers
+
 a =input()
 arr1 =list(map(int,input()))
 arry2 =list(map(int,input()))
